ed2fe98b70ca6f68ba98338158a73d39d139496e
Clayton O'Neill <clayton@oneill.net>
2025-10-21 11:53:45 -0400

    Migrate k2, k4, k5 to 10G single interface networking
    Remove LACP bond configuration (2×1G interfaces) and configure single
    10G interface (enp1s0f0) on all worker nodes.
    
    - Update Ansible network configs to use enp1s0f0 instead of bond0
    - Move DNS configuration from manage_resolv_conf to interface-level
    - Update UniFi DHCP reservations with physical 10G interface MACs
      - k2: b4:96:91:4b:34:58
      - k4: b4:96:91:a0:83:54
      - k5: b4:96:91:39:e0:94
    
    Network benchmarks confirm 7+ Gbps between all worker nodes.

--- opentofu/unifi.tf
+++ opentofu/unifi.tf
@@ -15,7 +15,7 @@ resource "unifi_user" "k1" {
 
 # k2 - Kubernetes node
 resource "unifi_user" "k2" {
-  mac              = "06:bc:96:51:3a:a0"
+  mac              = "b4:96:91:4b:34:58"
   name             = "k2"
   note             = "Kubernetes worker node"
   fixed_ip         = "172.19.74.112"
@@ -24,7 +24,7 @@ resource "unifi_user" "k2" {
 
 # k4 - Kubernetes node
 resource "unifi_user" "k4" {
-  mac              = "06:bc:96:51:3a:b1"
+  mac              = "b4:96:91:a0:83:54"
   name             = "k4"
   note             = "Kubernetes worker node"
   fixed_ip         = "172.19.74.75"
@@ -33,7 +33,7 @@ resource "unifi_user" "k4" {
 
 # k5 - Kubernetes node
 resource "unifi_user" "k5" {
-  mac              = "06:bc:96:51:3a:b0"
+  mac              = "b4:96:91:39:e0:94"
   name             = "k5"
   note             = "Kubernetes worker node"
   fixed_ip         = "172.19.74.76"
