0d1da87e6d23317b38d00fbe5c9949c6e0c548be
Clayton O'Neill <clayton@oneill.net>
2025-11-28 14:25:34 -0500

    Add k3 VM as Kubernetes worker
    - Add k3 VM to Proxmox (VM ID 1074, 4 vCPUs, 20GB RAM, 100GB local-zfs)
    - Add k3 to infrastructure_hosts for DHCP reservation and DNS
    - Add k3 to Ansible inventory and kubernetes_node group
    - Create host_vars for k3 network configuration
    - Fix preseed to include sudo package
    - Add lifecycle ignore_changes for node_name to allow VM migration

--- opentofu/proxmox.tf
+++ opentofu/proxmox.tf
@@ -53,4 +53,59 @@ resource "proxmox_virtual_environment_vm" "k1" {
   operating_system {
     type = "l26"
   }
+
+  lifecycle {
+    ignore_changes = [node_name, started]
+  }
+}
+
+resource "proxmox_virtual_environment_vm" "k3" {
+  name      = "k3"
+  node_name = "p9"
+  vm_id     = 1074
+
+  started = false
+  on_boot = true
+
+  cpu {
+    cores   = 4
+    sockets = 1
+    type    = "x86-64-v3"
+    units   = 100
+  }
+
+  memory {
+    dedicated = 20480
+  }
+
+  agent {
+    enabled = true
+    trim    = true
+  }
+
+  scsi_hardware = "virtio-scsi-single"
+
+  disk {
+    interface    = "scsi0"
+    datastore_id = "local-zfs"
+    size         = 100
+    file_format  = "raw"
+    iothread     = true
+    discard      = "on"
+  }
+
+  network_device {
+    bridge      = "vmbr0"
+    mac_address = "52:54:72:19:74:74"
+    model       = "virtio"
+    queues      = 4
+  }
+
+  operating_system {
+    type = "l26"
+  }
+
+  lifecycle {
+    ignore_changes = [node_name, started]
+  }
 }
