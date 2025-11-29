fcd50a90901fe32ba37a0c76d3e2ac19a519c44d
Clayton O'Neill <clayton@oneill.net>
2025-11-29 18:31:25 -0500

    Convert k2 to Proxmox host p2 and add cluster automation
    - Repurpose k2 hardware as Proxmox VE host p2 (172.19.74.42)
    - Add automated cluster join using community.proxmox modules
    - Add USB preparation script for automated Proxmox installs

--- opentofu/locals.tf
+++ opentofu/locals.tf
@@ -7,20 +7,30 @@
 #
 # This ensures UniFi DHCP and Route53 DNS stay automatically in sync.
 # To add a new host, simply add an entry here and run `opentofu apply`.
+#
+# Proxmox VE hosts IP plan: 172.19.74.4x (p1=.41, p2=.42, p3=.43, etc.)
 
 locals {
   infrastructure_hosts = {
+    # Proxmox VE hosts
+    p2 = {
+      mac      = "b4:96:91:4b:34:58"
+      ip       = "172.19.74.42"
+      hostname = "p2.oneill.net"
+      note     = "Proxmox VE host (formerly k2)"
+    }
+    p9 = {
+      mac      = "b4:96:91:39:e0:70"
+      ip       = "172.19.74.155"
+      hostname = "p9.oneill.net"
+      note     = "Proxmox VE host"
+    }
+    # Kubernetes nodes
     k1 = {
       mac      = "52:54:00:7a:16:72"
       ip       = "172.19.74.134"
       hostname = "k1.oneill.net"
       note     = "Kubernetes control-plane node (VM)"
-    }
-    k2 = {
-      mac      = "b4:96:91:4b:34:58"
-      ip       = "172.19.74.112"
-      hostname = "k2.oneill.net"
-      note     = "Kubernetes worker node"
     }
     k3 = {
       mac      = "52:54:72:19:74:74"
@@ -40,17 +50,12 @@ locals {
       hostname = "k5.oneill.net"
       note     = "Kubernetes worker node"
     }
+    # Other infrastructure
     fs2 = {
       mac      = "b4:96:91:4e:1b:ac"
       ip       = "172.19.74.139"
       hostname = "fs2.oneill.net"
       note     = "Synology NAS"
-    }
-    p9 = {
-      mac      = "b4:96:91:39:e0:70"
-      ip       = "172.19.74.155"
-      hostname = "p9.oneill.net"
-      note     = "Proxmox VE host"
     }
     infrapi = {
       mac      = "d8:3a:dd:1c:15:2f"
