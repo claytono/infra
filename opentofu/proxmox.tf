db8ff5d7020231c3963c55f83f2b24502f59e3a2
Clayton O'Neill <clayton@oneill.net>
2025-11-30 12:14:05 -0500

    Convert k4 to Proxmox host p4 and add k4 VM
    - Add p4 as new Proxmox VE host using k4's physical hardware
    - Create k4 VM resource on p4 (4 cores, 20GB RAM, 100GB disk)
    - Template Proxmox auto-install answer files instead of static files
    - Add AMT interface entries for p2, p4, k5 to DNS/DHCP
    - Fix cluster join to use preferred node's fingerprint (was using
      nodelist[0] which could mismatch in multi-node clusters)
    - Add custom default.toml that fails clearly for unknown hosts

--- opentofu/proxmox.tf
+++ opentofu/proxmox.tf
@@ -160,3 +160,54 @@ resource "proxmox_virtual_environment_vm" "k3" {
     ignore_changes = [node_name, started]
   }
 }
+
+resource "proxmox_virtual_environment_vm" "k4" {
+  name      = "k4"
+  node_name = "p4"
+  vm_id     = 1075
+
+  started = false
+  on_boot = true
+
+  cpu {
+    cores   = 4
+    sockets = 1
+    type    = "x86-64-v3"
+    units   = 100
+  }
+
+  memory {
+    dedicated = 20480
+  }
+
+  agent {
+    enabled = true
+    trim    = true
+  }
+
+  scsi_hardware = "virtio-scsi-single"
+
+  disk {
+    interface    = "scsi0"
+    datastore_id = "local-zfs"
+    size         = 100
+    file_format  = "raw"
+    iothread     = true
+    discard      = "on"
+  }
+
+  network_device {
+    bridge      = "vmbr0"
+    mac_address = "52:54:72:19:74:75"
+    model       = "virtio"
+    queues      = 4
+  }
+
+  operating_system {
+    type = "l26"
+  }
+
+  lifecycle {
+    ignore_changes = [node_name, started]
+  }
+}
