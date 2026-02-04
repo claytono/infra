8011a2925873ca88266db1e9dc2505efb37e684e
Clayton O'Neill <clayton@oneill.net>
2026-02-04 00:21:23 -0500

    Enable VLAN trunking on Proxmox hosts and configure SDN
    - Configure bridge_vids 2-4094 on Proxmox vmbr0 bridges
    - Create SDN VLAN zone and vnets for IOT (10), Guest (3), DMZ (78)
    - Add Proxmox Trunk switch port profile in UniFi
    - Configure switch port overrides for Proxmox hosts
    - Create DMZ network in UniFi (VLAN 78, 172.19.78.0/24)

--- opentofu/unifi.tf
+++ opentofu/unifi.tf
@@ -18,3 +18,60 @@ resource "unifi_user" "infrastructure_hosts" {
   fixed_ip         = each.value.ip
   local_dns_record = each.value.hostname
 }
+
+# --- Networks ---
+
+resource "unifi_network" "dmz" {
+  name    = "DMZ"
+  purpose = "corporate"
+
+  vlan_id      = 78
+  subnet       = "172.19.78.0/24"
+  dhcp_enabled = true
+  dhcp_start   = "172.19.78.129"
+  dhcp_stop    = "172.19.78.254"
+}
+
+# --- Switch port profiles ---
+
+data "unifi_network" "default" {
+  name = "Default"
+}
+
+resource "unifi_port_profile" "proxmox_trunk" {
+  name                  = "Proxmox Trunk"
+  forward               = "all"
+  native_networkconf_id = data.unifi_network.default.id
+}
+
+# --- US XG 16 switch ---
+# Import: tofu import unifi_device.usxg16 b4:fb:e4:56:ce:fe
+
+resource "unifi_device" "usxg16" {
+  mac  = "b4:fb:e4:56:ce:fe"
+  name = "US XG 16"
+
+  port_override {
+    number          = 1
+    name            = "p1"
+    port_profile_id = unifi_port_profile.proxmox_trunk.id
+  }
+
+  port_override {
+    number          = 2
+    name            = "p2"
+    port_profile_id = unifi_port_profile.proxmox_trunk.id
+  }
+
+  port_override {
+    number          = 4
+    name            = "p4"
+    port_profile_id = unifi_port_profile.proxmox_trunk.id
+  }
+
+  port_override {
+    number          = 5
+    name            = "p3"
+    port_profile_id = unifi_port_profile.proxmox_trunk.id
+  }
+}
