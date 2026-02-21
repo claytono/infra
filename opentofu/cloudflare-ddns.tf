908cb3f8b1d9745e06c86f9c5142399ed532265a
Clayton O'Neill <clayton@oneill.net>
2026-02-21 12:56:47 -0500

    Deploy cloudflare-ddns for luser.fnord.net
    - Mint scoped Cloudflare API token (DNS Write, fnord.net zone only)
    - Store token in 1Password for Kubernetes ExternalSecret
    - Deploy favonia/cloudflare-ddns in dedicated namespace
    - Export Cloudflare nameservers from DNS module

---
+++ opentofu/cloudflare-ddns.tf
@@ -0,0 +1,40 @@
+# Look up the "DNS Write" permission group ID
+data "cloudflare_account_api_token_permission_groups_list" "dns_write" {
+  account_id = local.cloudflare_account_id
+  name       = "DNS Write"
+}
+
+# Scoped Cloudflare API token for DDNS updates to fnord.net only
+resource "cloudflare_account_token" "ddns_fnord" {
+  account_id = local.cloudflare_account_id
+  name       = "cloudflare-ddns-fnord"
+
+  policies = [{
+    effect = "allow"
+    permission_groups = [{
+      id = data.cloudflare_account_api_token_permission_groups_list.dns_write.result[0].id
+    }]
+    resources = jsonencode({
+      "com.cloudflare.api.account.zone.${module.dns.cloudflare_fnord_net_zone_id}" = "*"
+    })
+  }]
+}
+
+# Store token in 1Password for Kubernetes ExternalSecret
+resource "onepassword_item" "cloudflare_ddns" {
+  vault    = data.onepassword_vault.infra.uuid
+  title    = "cloudflare-ddns"
+  category = "secure_note"
+
+  note_value = "Cloudflare API token for DDNS updates to fnord.net. Managed by OpenTofu - do not edit manually."
+
+  section {
+    label = "credentials"
+
+    field {
+      label = "CLOUDFLARE_API_TOKEN"
+      type  = "CONCEALED"
+      value = cloudflare_account_token.ddns_fnord.value
+    }
+  }
+}
