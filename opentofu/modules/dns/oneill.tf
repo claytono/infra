c9eb27310217da141eef5dd06b250c8d4fc1d6af
Clayton O'Neill <clayton@oneill.net>
2026-02-22 09:21:32 -0500

    Add Cloudflare zone and DNS records for oneill.net
    Mirror all static Route53 records on Cloudflare in preparation for
    nameserver cutover. Route53 remains authoritative until NS are updated
    at the registrar.
    
    - 1 zone + 56 DNS records (MX, TXT, CNAME, A, AAAA, NS)
    - CloudFront apex CNAME with Cloudflare CNAME flattening
    - k.oneill.net NS delegation to existing Route53 zone
    - Infrastructure host A/AAAA records via for_each
    - New outputs for zone ID and nameservers

--- opentofu/modules/dns/oneill.tf
+++ opentofu/modules/dns/oneill.tf
@@ -293,3 +293,243 @@ resource "aws_route53_record" "oneill_ns4_aaaa" {
   ttl     = 86400
   records = [local.nameserver_ips.ns4.ipv6]
 }
+
+# Cloudflare zone for oneill.net
+resource "cloudflare_zone" "oneill_net" {
+  account = {
+    id = var.cloudflare_account_id
+  }
+  name = "oneill.net"
+}
+
+# CloudFront distribution CNAMEs (apex uses Cloudflare CNAME flattening)
+resource "cloudflare_dns_record" "oneill_a" {
+  zone_id = cloudflare_zone.oneill_net.id
+  name    = "oneill.net"
+  type    = "CNAME"
+  content = "d330vtujw3sx24.cloudfront.net"
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "www" {
+  zone_id = cloudflare_zone.oneill_net.id
+  name    = "www.oneill.net"
+  type    = "CNAME"
+  content = "d330vtujw3sx24.cloudfront.net"
+  ttl     = 1
+}
+
+# MX records (one resource per server)
+resource "cloudflare_dns_record" "oneill_mx1" {
+  zone_id  = cloudflare_zone.oneill_net.id
+  name     = "oneill.net"
+  type     = "MX"
+  content  = "mx-1.pobox.com"
+  priority = 10
+  ttl      = 86400
+}
+
+resource "cloudflare_dns_record" "oneill_mx2" {
+  zone_id  = cloudflare_zone.oneill_net.id
+  name     = "oneill.net"
+  type     = "MX"
+  content  = "mx-2.pobox.com"
+  priority = 10
+  ttl      = 86400
+}
+
+resource "cloudflare_dns_record" "oneill_mx3" {
+  zone_id  = cloudflare_zone.oneill_net.id
+  name     = "oneill.net"
+  type     = "MX"
+  content  = "mx-3.pobox.com"
+  priority = 10
+  ttl      = 86400
+}
+
+# SPF TXT record
+resource "cloudflare_dns_record" "oneill_spf" {
+  zone_id = cloudflare_zone.oneill_net.id
+  name    = "oneill.net"
+  type    = "TXT"
+  content = "\"v=spf1 include:_spf.google.com include:spf.messagingengine.com ~all\""
+  ttl     = 3600
+}
+
+# DKIM TXT record (MessageSystems)
+resource "cloudflare_dns_record" "dkim" {
+  zone_id = cloudflare_zone.oneill_net.id
+  name    = "mesmtp._domainkey.oneill.net"
+  type    = "TXT"
+  content = "\"v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCSWE5uk2EOlSVQ2Z68vMr04EQ5NoC0ki3wIDY3zIXFaEGbPisEJEYsNQ6fbj+d+9sc6kZ079M77S/FNpgZuWDepqZyT5SmzwGMw0RbUPr3F1JvQ9wFVx15P2ssPrFiY1Lv9vskqvanDka5+TDC7oiUd9oFZanF/KVLxMNsRRtStQIDAQAB\""
+  ttl     = 3600
+}
+
+# DKIM CNAMEs (Fastmail)
+resource "cloudflare_dns_record" "dkim_fm1" {
+  zone_id = cloudflare_zone.oneill_net.id
+  name    = "fm1._domainkey.oneill.net"
+  type    = "CNAME"
+  content = "fm1.oneill.net.dkim.fmhosted.com"
+  ttl     = 3600
+}
+
+resource "cloudflare_dns_record" "dkim_fm2" {
+  zone_id = cloudflare_zone.oneill_net.id
+  name    = "fm2._domainkey.oneill.net"
+  type    = "CNAME"
+  content = "fm2.oneill.net.dkim.fmhosted.com"
+  ttl     = 3600
+}
+
+resource "cloudflare_dns_record" "dkim_fm3" {
+  zone_id = cloudflare_zone.oneill_net.id
+  name    = "fm3._domainkey.oneill.net"
+  type    = "CNAME"
+  content = "fm3.oneill.net.dkim.fmhosted.com"
+  ttl     = 3600
+}
+
+# ACM SSL certificate validation CNAMEs
+resource "cloudflare_dns_record" "acm_validation" {
+  zone_id = cloudflare_zone.oneill_net.id
+  name    = "_aab5ac87821783e4e8f7de6297024cbf.oneill.net"
+  type    = "CNAME"
+  content = "_1a2c7c1f07a03bf47cb635883858422f.lblqlwmygg.acm-validations.aws"
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "www_acm_validation" {
+  zone_id = cloudflare_zone.oneill_net.id
+  name    = "_60263719a5bd132a7a8af2a903116405.www.oneill.net"
+  type    = "CNAME"
+  content = "_d7fef7f4941ad7aa5ae1136c4cd91060.lblqlwmygg.acm-validations.aws"
+  ttl     = 1
+}
+
+# Bluesky AT Protocol verification
+resource "cloudflare_dns_record" "atproto" {
+  zone_id = cloudflare_zone.oneill_net.id
+  name    = "_atproto.oneill.net"
+  type    = "TXT"
+  content = "\"did=did:plc:cvvzqrdxxordkaboxirrerb3\""
+  ttl     = 1
+}
+
+# GitHub Pages CNAME
+resource "cloudflare_dns_record" "clayton" {
+  zone_id = cloudflare_zone.oneill_net.id
+  name    = "clayton.oneill.net"
+  type    = "CNAME"
+  content = "claytono.github.io"
+  ttl     = 3600
+}
+
+# Proxmox auto-installer URL discovery
+resource "cloudflare_dns_record" "proxmox_auto_installer" {
+  zone_id = cloudflare_zone.oneill_net.id
+  name    = "proxmox-auto-installer.oneill.net"
+  type    = "TXT"
+  content = "\"https://os-install.oneill.net/answer\""
+  ttl     = 1
+}
+
+# Router A record
+resource "cloudflare_dns_record" "router" {
+  zone_id = cloudflare_zone.oneill_net.id
+  name    = "router.oneill.net"
+  type    = "A"
+  content = "172.19.74.1"
+  ttl     = 3600
+}
+
+# k.oneill.net NS delegation (stays on Route53, managed by external-dns)
+resource "cloudflare_dns_record" "k_subdomain_ns" {
+  for_each = toset(aws_route53_zone.k_oneill_net.name_servers)
+  zone_id  = cloudflare_zone.oneill_net.id
+  name     = "k.oneill.net"
+  type     = "NS"
+  content  = each.value
+  ttl      = 3600
+}
+
+# Infrastructure hosts - A records
+resource "cloudflare_dns_record" "cf_infrastructure_hosts" {
+  for_each = { for k, v in var.infrastructure_hosts : k => v if v.public_dns }
+  zone_id  = cloudflare_zone.oneill_net.id
+  name     = each.value.hostname
+  type     = "A"
+  content  = each.value.ip
+  ttl      = 1
+}
+
+# Infrastructure hosts - AAAA records (IPv6-enabled hosts only)
+resource "cloudflare_dns_record" "cf_infrastructure_hosts_aaaa" {
+  for_each = {
+    for k, v in var.infrastructure_hosts : k => v
+    if v.public_dns && v.enable_ipv6
+  }
+  zone_id = cloudflare_zone.oneill_net.id
+  name    = each.value.hostname
+  type    = "AAAA"
+  content = format("%s::74:%s", var.infrastructure_ipv6_prefix, split(".", each.value.ip)[3])
+  ttl     = 1
+}
+
+# Service CNAMEs for infra1 services
+resource "cloudflare_dns_record" "nut" {
+  zone_id = cloudflare_zone.oneill_net.id
+  name    = "nut.oneill.net"
+  type    = "CNAME"
+  content = "infra1.oneill.net"
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "os_install" {
+  zone_id = cloudflare_zone.oneill_net.id
+  name    = "os-install.oneill.net"
+  type    = "CNAME"
+  content = "infra1.oneill.net"
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "pve" {
+  zone_id = cloudflare_zone.oneill_net.id
+  name    = "pve.oneill.net"
+  type    = "CNAME"
+  content = "infra1.oneill.net"
+  ttl     = 1
+}
+
+# Service CNAMEs for pantrypi services
+resource "cloudflare_dns_record" "zwavejs" {
+  zone_id = cloudflare_zone.oneill_net.id
+  name    = "zwavejs.oneill.net"
+  type    = "CNAME"
+  content = "pantrypi.oneill.net"
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "zigbee2mqtt" {
+  zone_id = cloudflare_zone.oneill_net.id
+  name    = "zigbee2mqtt.oneill.net"
+  type    = "CNAME"
+  content = "pantrypi.oneill.net"
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "matter" {
+  zone_id = cloudflare_zone.oneill_net.id
+  name    = "matter.oneill.net"
+  type    = "CNAME"
+  content = "pantrypi.oneill.net"
+  ttl     = 1
+}
+
+resource "cloudflare_dns_record" "thread" {
+  zone_id = cloudflare_zone.oneill_net.id
+  name    = "thread.oneill.net"
+  type    = "CNAME"
+  content = "pantrypi.oneill.net"
+  ttl     = 1
+}
--- opentofu/modules/dns/outputs.tf
+++ opentofu/modules/dns/outputs.tf
@@ -24,3 +24,13 @@ output "cloudflare_fnord_net_nameservers" {
   description = "Cloudflare nameservers for fnord.net"
   value       = cloudflare_zone.fnord_net.name_servers
 }
+
+output "cloudflare_oneill_net_zone_id" {
+  description = "Cloudflare zone ID for oneill.net"
+  value       = cloudflare_zone.oneill_net.id
+}
+
+output "cloudflare_oneill_net_nameservers" {
+  description = "Cloudflare nameservers for oneill.net"
+  value       = cloudflare_zone.oneill_net.name_servers
+}
