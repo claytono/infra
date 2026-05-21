6b81fba53b5b4e1c3845ad7a6803689a60d76147
Clayton O'Neill <clayton@oneill.net>
2026-05-20 23:51:09 -0400

    Update GitHub provider secret arguments
    Bumps the integrations/github provider lockfile to 6.12.1 and migrates github_actions_secret resources from the deprecated plaintext_value argument to value.
    
    A live OpenTofu plan shows 12 in-place github_actions_secret updates for the argument rename, with 0 to add and 0 to destroy.

--- opentofu/github-repos.tf
+++ opentofu/github-repos.tf
@@ -9,55 +9,55 @@ locals {
 
 # Infra repository secrets
 resource "github_actions_secret" "infra_argocd_auth_token" {
-  repository      = "infra"
-  secret_name     = "ARGOCD_AUTH_TOKEN"
-  plaintext_value = data.onepassword_item.argocd_github_actions_token.password
+  repository  = "infra"
+  secret_name = "ARGOCD_AUTH_TOKEN"
+  value       = data.onepassword_item.argocd_github_actions_token.password
 }
 
 resource "github_actions_secret" "infra_cachix_auth_token" {
-  repository      = "infra"
-  secret_name     = "CACHIX_AUTH_TOKEN"
-  plaintext_value = data.onepassword_item.cachix_auth_token.password
+  repository  = "infra"
+  secret_name = "CACHIX_AUTH_TOKEN"
+  value       = data.onepassword_item.cachix_auth_token.password
 }
 
 resource "github_actions_secret" "infra_tailscale_oauth_client_id" {
-  repository      = "infra"
-  secret_name     = "TAILSCALE_OAUTH_CLIENT_ID"
-  plaintext_value = tailscale_oauth_client.github_actions.id
+  repository  = "infra"
+  secret_name = "TAILSCALE_OAUTH_CLIENT_ID"
+  value       = tailscale_oauth_client.github_actions.id
 }
 
 resource "github_actions_secret" "infra_tailscale_oauth_client_secret" {
-  repository      = "infra"
-  secret_name     = "TAILSCALE_OAUTH_CLIENT_SECRET"
-  plaintext_value = tailscale_oauth_client.github_actions.key
+  repository  = "infra"
+  secret_name = "TAILSCALE_OAUTH_CLIENT_SECRET"
+  value       = tailscale_oauth_client.github_actions.key
 }
 
 resource "github_actions_secret" "tailscale_ssh_oauth_client_id" {
   for_each = local.tailscale_ssh_github_repositories
 
-  repository      = each.key
-  secret_name     = "TAILSCALE_SSH_OAUTH_CLIENT_ID"
-  plaintext_value = tailscale_oauth_client.github_actions_ssh.id
+  repository  = each.key
+  secret_name = "TAILSCALE_SSH_OAUTH_CLIENT_ID"
+  value       = tailscale_oauth_client.github_actions_ssh.id
 }
 
 resource "github_actions_secret" "tailscale_ssh_oauth_client_secret" {
   for_each = local.tailscale_ssh_github_repositories
 
-  repository      = each.key
-  secret_name     = "TAILSCALE_SSH_OAUTH_CLIENT_SECRET"
-  plaintext_value = tailscale_oauth_client.github_actions_ssh.key
+  repository  = each.key
+  secret_name = "TAILSCALE_SSH_OAUTH_CLIENT_SECRET"
+  value       = tailscale_oauth_client.github_actions_ssh.key
 }
 
 resource "github_actions_secret" "infra_semaphore_api_token" {
-  repository      = "infra"
-  secret_name     = "SEMAPHORE_API_TOKEN"
-  plaintext_value = local.semaphore_api_token
+  repository  = "infra"
+  secret_name = "SEMAPHORE_API_TOKEN"
+  value       = local.semaphore_api_token
 }
 
 resource "github_actions_secret" "infra_claude_code_oauth_token" {
-  repository      = "infra"
-  secret_name     = "CLAUDE_CODE_OAUTH_TOKEN"
-  plaintext_value = data.onepassword_item.claude_code_oauth_token.credential
+  repository  = "infra"
+  secret_name = "CLAUDE_CODE_OAUTH_TOKEN"
+  value       = data.onepassword_item.claude_code_oauth_token.credential
 }
 
 resource "github_actions_variable" "infra_semaphore_project" {
@@ -74,13 +74,13 @@ resource "github_actions_variable" "infra_semaphore_template" {
 
 # Website-Hugo repository secrets (Cloudflare Pages deployment)
 resource "github_actions_secret" "website_hugo_cf_api_token" {
-  repository      = "website-hugo"
-  secret_name     = "CLOUDFLARE_API_TOKEN"
-  plaintext_value = cloudflare_account_token.pages_deploy.value
+  repository  = "website-hugo"
+  secret_name = "CLOUDFLARE_API_TOKEN"
+  value       = cloudflare_account_token.pages_deploy.value
 }
 
 resource "github_actions_secret" "website_hugo_cf_account_id" {
-  repository      = "website-hugo"
-  secret_name     = "CLOUDFLARE_ACCOUNT_ID"
-  plaintext_value = local.cloudflare_account_id
+  repository  = "website-hugo"
+  secret_name = "CLOUDFLARE_ACCOUNT_ID"
+  value       = local.cloudflare_account_id
 }
